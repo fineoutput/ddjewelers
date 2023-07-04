26e03d24534559046c8a314b046fa0b5c24407bc
Divyanshu <divyanshu6800@gmail.com>
2023-07-04 17:28:27 +0530

    update

--- application/controllers/f9admin/State.php
+++ application/controllers/f9admin/State.php
@@ -1,308 +1,267 @@
-<?php
-     if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-        require_once(APPPATH . 'core/CI_finecontrol.php');
-        class State extends CI_finecontrol{
-        function __construct()
-            {
-              parent::__construct();
-              $this->load->model("login_model");
-              $this->load->model("admin/base_model");
-              $this->load->library('user_agent');
-              $this->load->library('upload');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+require_once(APPPATH . 'core/CI_finecontrol.php');
+class State extends CI_finecontrol
+{
+  function __construct()
+  {
+    parent::__construct();
+    $this->load->model("login_model");
+    $this->load->model("admin/base_model");
+    $this->load->library('user_agent');
+    $this->load->library('upload');
+  }
 
-             
-            }
+  public function view_state()
+  {
 
-          public function view_state(){
-
-             if(!empty($this->session->userdata('admin_data'))){
-
-
-               $data['user_name']=$this->load->get_var('user_name');
-
-               // echo SITE_NAME;
-               // echo $this->session->userdata('image');
-               // echo $this->session->userdata('position');
-               // exit;
-
-                            $this->db->select('*');
-                $this->db->from('tbl_state');
-                //$this->db->where('id',$usr);
-                $data['state_data']= $this->db->get();
-
-               $this->load->view('admin/common/header_view',$data);
-               $this->load->view('admin/state/view_state');
-               $this->load->view('admin/common/footer_view');
-
-           }
-           else{
-
-              redirect("login/admin_login","refresh");
-           }
-
-           }
-
-               public function add_state(){
-
-                  if(!empty($this->session->userdata('admin_data'))){
-
-                    $this->load->view('admin/common/header_view');
-                    $this->load->view('admin/state/add_state');
-                    $this->load->view('admin/common/footer_view');
-
-                }
-                else{
-
-                   redirect("login/admin_login","refresh");
-                }
-
-                }
-
-                public function update_state($idd){
-
-                    if(!empty($this->session->userdata('admin_data'))){
+    if (!empty($this->session->userdata('admin_data'))) {
 
 
-                      $data['user_name']=$this->load->get_var('user_name');
+      $data['user_name'] = $this->load->get_var('user_name');
 
-                      // echo SITE_NAME;
-                      // echo $this->session->userdata('image');
-                      // echo $this->session->userdata('position');
-                      // exit;
+      // echo SITE_NAME;
+      // echo $this->session->userdata('image');
+      // echo $this->session->userdata('position');
+      // exit;
 
-                       $id=base64_decode($idd);
-                      $data['id']=$idd;
+      $this->db->select('*');
+      $this->db->from('tbl_state');
+      //$this->db->where('id',$usr);
+      $data['state_data'] = $this->db->get();
 
-                             $this->db->select('*');
-                             $this->db->from('tbl_state');
-                             $this->db->where('id',$id);
-                             $data['state_data']= $this->db->get()->row();
+      $this->load->view('admin/common/header_view', $data);
+      $this->load->view('admin/state/view_state');
+      $this->load->view('admin/common/footer_view');
+    } else {
+
+      redirect("login/admin_login", "refresh");
+    }
+  }
+
+  public function add_state()
+  {
+
+    if (!empty($this->session->userdata('admin_data'))) {
+
+      $this->load->view('admin/common/header_view');
+      $this->load->view('admin/state/add_state');
+      $this->load->view('admin/common/footer_view');
+    } else {
+
+      redirect("login/admin_login", "refresh");
+    }
+  }
+
+  public function update_state($idd)
+  {
+
+    if (!empty($this->session->userdata('admin_data'))) {
 
 
-                      $this->load->view('admin/common/header_view',$data);
-                      $this->load->view('admin/state/update_state');
-                      $this->load->view('admin/common/footer_view');
+      $data['user_name'] = $this->load->get_var('user_name');
 
-                  }
-                  else{
+      // echo SITE_NAME;
+      // echo $this->session->userdata('image');
+      // echo $this->session->userdata('position');
+      // exit;
 
-                     redirect("login/admin_login","refresh");
-                  }
+      $id = base64_decode($idd);
+      $data['id'] = $idd;
 
-                  }
-
-              public function add_state_data($t,$iw="")
-
-                {
-
-                  if(!empty($this->session->userdata('admin_data'))){
+      $this->db->select('*');
+      $this->db->from('tbl_state');
+      $this->db->where('id', $id);
+      $data['state_data'] = $this->db->get()->row();
 
 
-              $this->load->helper(array('form', 'url'));
-              $this->load->library('form_validation');
-              $this->load->helper('security');
-              if($this->input->post())
-              {
-                // print_r($this->input->post());
-                // exit;
- 	 $this->form_validation->set_rules('name', 'name', 'required');
+      $this->load->view('admin/common/header_view', $data);
+      $this->load->view('admin/state/update_state');
+      $this->load->view('admin/common/footer_view');
+    } else {
 
-          
-          
-        
-        
-                if($this->form_validation->run()== TRUE)
-                {
- 	 $name=$this->input->post('name'); 
+      redirect("login/admin_login", "refresh");
+    }
+  }
 
-                    $ip = $this->input->ip_address();
-                    date_default_timezone_set("Asia/Calcutta");
-                    $cur_date=date("Y-m-d H:i:s");
-                    $addedby=$this->session->userdata('admin_id');
+  public function add_state_data($t, $iw = "")
 
-            $typ=base64_decode($t);
-            $last_id = 0;
-            if($typ==1){
+  {
+
+    if (!empty($this->session->userdata('admin_data'))) {
+
+
+      $this->load->helper(array('form', 'url'));
+      $this->load->library('form_validation');
+      $this->load->helper('security');
+      if ($this->input->post()) {
+        // print_r($this->input->post());
+        // exit;
+        $this->form_validation->set_rules('name', 'name', 'required');
+
+
+
+
+
+        if ($this->form_validation->run() == TRUE) {
+          $name = $this->input->post('name');
+
+          $ip = $this->input->ip_address();
+          date_default_timezone_set("Asia/Calcutta");
+          $cur_date = date("Y-m-d H:i:s");
+          $addedby = $this->session->userdata('admin_id');
+
+          $typ = base64_decode($t);
+          $last_id = 0;
+          if ($typ == 1) {
 
 
 
             $data_insert = array(
-                 	 'name'=>$name, 
+              'name' => $name,
 
-                      'ip' =>$ip,
-                      'added_by' =>$addedby,
-                      'is_active' =>1,
-                      'date'=>$cur_date
-                      );
-
-
-            $last_id=$this->base_model->insert_table("tbl_state",$data_insert,1) ;
-
-            }
-            if($typ==2){
-
-     $idw=base64_decode($iw);
+              'ip' => $ip,
+              'added_by' => $addedby,
+              'is_active' => 1,
+              'date' => $cur_date
+            );
 
 
-  $this->db->select('*');
-  $this->db->from('tbl_state');
-  $this->db->where('id',$idw);
-  $dsa=$this->db->get();
-  $da=$dsa->row();
+            $last_id = $this->base_model->insert_table("tbl_state", $data_insert, 1);
+          }
+          if ($typ == 2) {
 
-  
+            $idw = base64_decode($iw);
 
-  
+
+            $this->db->select('*');
+            $this->db->from('tbl_state');
+            $this->db->where('id', $idw);
+            $dsa = $this->db->get();
+            $da = $dsa->row();
+
+
+
+
 
             $data_insert = array(
-                 	 'name'=>$name, 
+              'name' => $name,
 
-                      );
-              $this->db->where('id', $idw);
-              $last_id=$this->db->update('tbl_state', $data_insert);
-            }
-                        if($last_id!=0){
-                                $this->session->set_flashdata('smessage','Data inserted successfully');
-                                redirect("dcadmin/State/view_state","refresh");
-                               }
-                                else
-                                    {
+            );
+            $this->db->where('id', $idw);
+            $last_id = $this->db->update('tbl_state', $data_insert);
+          }
+          if ($last_id != 0) {
+            $this->session->set_flashdata('smessage', 'Data inserted successfully');
+            redirect("dcadmin/State/view_state", "refresh");
+          } else {
 
-                                     $this->session->set_flashdata('emessage','Sorry error occured');
-                                     redirect($_SERVER['HTTP_REFERER']);
-                                   }
-                }
-              else{
+            $this->session->set_flashdata('emessage', 'Sorry error occured');
+            redirect($_SERVER['HTTP_REFERER']);
+          }
+        } else {
 
-         $this->session->set_flashdata('emessage',validation_errors());
-       redirect($_SERVER['HTTP_REFERER']);
+          $this->session->set_flashdata('emessage', validation_errors());
+          redirect($_SERVER['HTTP_REFERER']);
+        }
+      } else {
 
-              }
+        $this->session->set_flashdata('emessage', 'Please insert some data, No data available');
+        redirect($_SERVER['HTTP_REFERER']);
+      }
+    } else {
 
-              }
-            else{
+      redirect("login/admin_login", "refresh");
+    }
+  }
 
-  $this->session->set_flashdata('emessage','Please insert some data, No data available');
-       redirect($_SERVER['HTTP_REFERER']);
+  public function updatestateStatus($idd, $t)
+  {
 
-            }
-            }
-            else{
-
-        redirect("login/admin_login","refresh");
-
-
-            }
-
-            }
-
-                public function updatestateStatus($idd,$t){
-
-                         if(!empty($this->session->userdata('admin_data'))){
+    if (!empty($this->session->userdata('admin_data'))) {
 
 
-                           $data['user_name']=$this->load->get_var('user_name');
+      $data['user_name'] = $this->load->get_var('user_name');
 
-                           // echo SITE_NAME;
-                           // echo $this->session->userdata('image');
-                           // echo $this->session->userdata('position');
-                           // exit;
-                           $id=base64_decode($idd);
+      // echo SITE_NAME;
+      // echo $this->session->userdata('image');
+      // echo $this->session->userdata('position');
+      // exit;
+      $id = base64_decode($idd);
 
-                           if($t=="active"){
+      if ($t == "active") {
 
-                             $data_update = array(
-                         'is_active'=>1
+        $data_update = array(
+          'is_active' => 1
 
-                         );
+        );
 
-                         $this->db->where('id', $id);
-                        $zapak=$this->db->update('tbl_state', $data_update);
+        $this->db->where('id', $id);
+        $zapak = $this->db->update('tbl_state', $data_update);
 
-                             if($zapak!=0){
-                             redirect("dcadmin/State/view_state","refresh");
-                                     }
-                                     else
-                                     {
-         $this->session->set_flashdata('emessage','Sorry error occured');
-           redirect($_SERVER['HTTP_REFERER']);
-                                     }
-                           }
-                           if($t=="inactive"){
-                             $data_update = array(
-                          'is_active'=>0
+        if ($zapak != 0) {
+          redirect("dcadmin/State/view_state", "refresh");
+        } else {
+          $this->session->set_flashdata('emessage', 'Sorry error occured');
+          redirect($_SERVER['HTTP_REFERER']);
+        }
+      }
+      if ($t == "inactive") {
+        $data_update = array(
+          'is_active' => 0
 
-                          );
+        );
 
-                          $this->db->where('id', $id);
-                          $zapak=$this->db->update('tbl_state', $data_update);
+        $this->db->where('id', $id);
+        $zapak = $this->db->update('tbl_state', $data_update);
 
-                              if($zapak!=0){
-                              redirect("dcadmin/State/view_state","refresh");
-                                      }
-                                      else
-                                      {
+        if ($zapak != 0) {
+          redirect("dcadmin/State/view_state", "refresh");
+        } else {
 
-                 $this->session->set_flashdata('emessage','Sorry error occured');
-                   redirect($_SERVER['HTTP_REFERER']);
-                                      }
-                           }
+          $this->session->set_flashdata('emessage', 'Sorry error occured');
+          redirect($_SERVER['HTTP_REFERER']);
+        }
+      }
+    } else {
+
+      redirect("login/admin_login", "refresh");
+    }
+  }
 
 
 
-                       }
-                       else{
+  public function delete_state($idd)
+  {
 
-                          redirect("login/admin_login","refresh");
+    if (!empty($this->session->userdata('admin_data'))) {
 
-                       }
+      $data['user_name'] = $this->load->get_var('user_name');
 
-                       }
+      // echo SITE_NAME;
+      // echo $this->session->userdata('image');
+      // echo $this->session->userdata('position');
+      // exit;
+      $id = base64_decode($idd);
 
-
-
-                public function delete_state($idd){
-
-                       if(!empty($this->session->userdata('admin_data'))){
-
-                         $data['user_name']=$this->load->get_var('user_name');
-
-                         // echo SITE_NAME;
-                         // echo $this->session->userdata('image');
-                         // echo $this->session->userdata('position');
-                         // exit;
-                         $id=base64_decode($idd);
-
-                        if($this->load->get_var('position')=="Super Admin"){
-
-                    
-
-  $zapak=$this->db->delete('tbl_state', array('id' => $id));
-  if($zapak!=0){
-         
-         redirect("dcadmin/State/view_state","refresh");
-                 }
-                 else
-                 {
-                    $this->session->set_flashdata('emessage','Sorry error occured');
-                    redirect($_SERVER['HTTP_REFERER']);
-                 }
-             }
-             else{
-              $this->session->set_flashdata('emessage','Sorry you not a super admin you dont have permission to delete anything');
-                redirect($_SERVER['HTTP_REFERER']);
-             }
+      if ($this->load->get_var('position') == "Super Admin") {
 
 
-                             }
-                             else{
 
-                         redirect("login/admin_login","refresh");
+        $zapak = $this->db->delete('tbl_state', array('id' => $id));
+        if ($zapak != 0) {
 
-                             }
+          redirect("dcadmin/State/view_state", "refresh");
+        } else {
+          $this->session->set_flashdata('emessage', 'Sorry error occured');
+          redirect($_SERVER['HTTP_REFERER']);
+        }
+      } else {
+        $this->session->set_flashdata('emessage', 'Sorry you not a super admin you dont have permission to delete anything');
+        redirect($_SERVER['HTTP_REFERER']);
+      }
+    } else {
 
-                             }
-                       }
-
-       ?>
-        
+      redirect("login/admin_login", "refresh");
+    }
+  }
+}
