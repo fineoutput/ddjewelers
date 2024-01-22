9829d178b9fda9446003d0e4553d4a3d33129253
Nitesh Shah <niteshshah000@gmail.com>
2024-01-22 19:25:48 +0530

    updates

--- application/views/all_products.php
+++ application/views/all_products.php
@@ -131,7 +131,7 @@
         <div class="row">
           <div class="col-md-12 mb-4">
             <div class="row ">
-              <div class="col-md-8 mb-4 hrds">
+              <div class="col-md-12 mb-4 hrds">
                 <h1 class="r-title">
                   <?= $heading . " ( " . $productCount . " )"; ?>
                 </h1>
